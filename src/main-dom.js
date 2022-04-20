5b43737bec91abcfbfe4b8224ba4a59b763d3828
zurbash <duta1101@bk.ru>
2022-04-20 14:53:47 +0400

    added header contents and styling

--- src/main-dom.js
+++ src/main-dom.js
@@ -1,14 +1,28 @@
 const content = document.querySelector(".content");
-function header(){
+function createHeader(){
     const header = document.createElement('div');
     header.classList.add("header");
     content.appendChild(header);
+    headerContent(header);
 }
+function headerContent(header){
+    const heading = document.createElement("h1");
+    heading.textContent="Todo List";
+    heading.classList.add("heading");
+    header.appendChild(heading);
+    const searchBar = document.createElement("input");
+    searchBar.type="search";
+    searchBar.placeholder="search";
+    searchBar.classList.add("searchBar");
+    header.appendChild(searchBar);
+}
+
 function sidebar(){
     const sidebar = document.createElement('div');
     sidebar.classList.add("sidebar");
     content.appendChild(sidebar);
 }
+
 function contentSpace(){
     const mainContent = document.createElement('div');
     mainContent.classList.add("mainContent");
@@ -16,7 +30,7 @@ function contentSpace(){
 }
 
 function createMain(){
-    header();
+    createHeader();
     sidebar();
     contentSpace();
 }
