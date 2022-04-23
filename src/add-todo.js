68f66fd82d62611f7c76d02cfdf677a7c1a150e5
zurbash <duta1101@bk.ru>
2022-04-23 20:27:09 +0400

    added script for removing individual todo tasks

--- src/add-todo.js
+++ src/add-todo.js
@@ -1,4 +1,5 @@
 import taskPush from "./create-task"
+import { formatDistanceToNow } from 'date-fns'
 let todoArr = [];
 let taskArr = [];
 let activeId;
@@ -46,21 +47,44 @@ function createCard(i){
     let description = document.createElement("p");
     description.textContent=obj.description;
     let priority = obj.priority;
+    let deleteCard = document.createElement("button")
+    deleteCard.classList.add("deleteCard");
+    deleteCard.textContent="Delete";
+    deleteCard.addEventListener("click", ()=>{ cardRemove(card,i)})
+    let date = document.createElement("p");
+    date.textContent=`Due Date ${obj.dueDate}`;
     setPriority(card,priority);
-    appendChild(card,title,description)
+    appendChild(card,title,description,date,deleteCard)
 
 
 }
+function cardRemove(card){
+    const activeTask = document.querySelector(".activeClass");
+    const cards = document.querySelectorAll(".card");
+    for(let i=0;i<todoArr.length;i++){
+        cards[i].setAttribute("id",i);
+    }
+    let i = parseInt(card.id);;
+    let id = parseInt(activeTask.id);
+    taskArr[id].splice(i,1);
+    todoArr
+    card.remove();
+    console.log();
+    console.log(taskArr);
+}
+//sets priority color on cards
 function setPriority(card, priority){
     if (priority=="High"){card.style.borderLeft="10px solid rgb(201, 9, 9)"}
     else if (priority=="Medium"){card.style.borderLeft="10px solid yellow"}
     else{card.style.borderLeft="10px solid rgb(8, 153, 8)"}
 }
-function appendChild(card,title,description){
+function appendChild(card,title,description,date,deleteCard){
     const container = document.querySelector(".mainContent");
     container.appendChild(card)
     card.appendChild(title)
     card.appendChild(description)
+    card.appendChild(date)
+    card.appendChild(deleteCard)
 }
 //removes cards from dom
 function empty(element) {
